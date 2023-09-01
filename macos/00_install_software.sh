5986328b50788771ff21e9e8ccbf8c09cdc3dd68
katabame <ayame@kataba.me>
2023-09-01 14:21:02 +0900

    Update: [macos] setup scripts 🧩

--- macos/00_install_software.sh
+++ macos/00_install_software.sh
@@ -1,39 +1,41 @@
 #!/bin/bash
 set -e
 
-echo '               init.kataba.me Phase 1'
-if [ -e /Library/Apple/usr/libexec/oah/RosettaLinux/rosetta ]; then
-    echo 'Found Rosetta2'
-else
+
+if [ ! -e /Library/Apple/usr/libexec/oah/RosettaLinux/rosetta ]; then
+    echo '               init.kataba.me Phase 1 / 4'
     echo 'Please type following command to install Rosetta2'
     echo 'sudo softwareupdate --install-rosetta'
     exit 1
 fi
 
-echo.
-
-echo '               init.kataba.me Phase 2'
-if [ -e /opt/homebrew/bin/brew ]; then
-    brew_formulae=(`curl -fsSLk https://init.kataba.me/mac/brew-formulae | grep -v '#' | grep -v '^$'`)
-    brew_casks=(`curl -fsSLk https://init.kataba.me/mac/brew-casks | grep -v '#' | grep -v '^$'`)
-    #mas_packages=(`curl -fsSLk https://init.kataba.me/mac/mas-packages | grep -v '#' | grep -v '^$'`)
-
-    for formula in ${brew_formulae[@]}; do
-        NONINTERACTIVE=1 HOMEBREW_NO_INSTALL_CLEANUP=1 /opt/homebrew/bin/brew install --quiet ${formula}
-    done
-
-    for cask in ${brew_casks[@]}; do
-        NONINTERACTIVE=1 HOMEBREW_NO_INSTALL_CLEANUP=1 /opt/homebrew/bin/brew install --quiet --cask ${cask}
-    done
-
-    #for mas in ${mas_packages[@]}; do
-    #    NONINTERACTIVE=1 HOMEBREW_NO_INSTALL_CLEANUP=1 /opt/homebrew/bin/mas install ${mas}
-    #done
-else
+if [ ! -e /opt/homebrew/bin/brew ]; then
+    echo '               init.kataba.me Phase 2 / 4'
     echo 'Please follow instructions to install brew'
     curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh > ~/brew.sh
     chmod +x ~/brew.sh
     NONINTERACTIVE=1 ~/brew.sh
     rm ~/brew.sh
     exit 1
+else
+    echo '               init.kataba.me Phase 3 / 4'
+    brew_formulae=(`curl -fsSLk https://init.kataba.me/mac/brew-formulae | grep -v '#' | grep -v '^$'`)
+    brew_casks=(`curl -fsSLk https://init.kataba.me/mac/brew-casks | grep -v '#' | grep -v '^$'`)
+    #mas_packages=(`curl -fsSLk https://init.kataba.me/mac/mas-packages | grep -v '#' | grep -v '^$'`)
+
+    for formula in ${brew_formulae[@]}; do
+        NONINTERACTIVE=1 HOMEBREW_NO_INSTALL_CLEANUP=1 /opt/homebrew/bin/brew install --quiet ${formula} 2> /dev/null
+    done
+
+    for cask in ${brew_casks[@]}; do
+        NONINTERACTIVE=1 HOMEBREW_NO_INSTALL_CLEANUP=1 /opt/homebrew/bin/brew install --quiet --cask ${cask} 2> /dev/null
+    done
+
+    #for mas in ${mas_packages[@]}; do
+    #    NONINTERACTIVE=1 HOMEBREW_NO_INSTALL_CLEANUP=1 /opt/homebrew/bin/mas install ${mas}
+    #done
+    exit 1
 fi
+
+echo
+echo '               init.kataba.me Phase 4 / 4'
